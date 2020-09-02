c429e142d1c991df1d1bff3f82803b293f9654c3
kitsune <57214464+Terr0rize@users.noreply.github.com>
2020-09-02 16:57:29 +0400

    Add files via upload

---
+++ C sharp learning/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace C_sharp_learning
+{
+    class MainClass
+    {
+        static void Main(string[] args) //метод срабатывает при старте программы
+        {
+            Random randNum = new Random();
+
+
+            int num;
+            num = randNum.Next(1, 6); //рандом от 1 до 5
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Num is - " + num);
+                    break;
+                case 2:
+                    Console.WriteLine("Num is - " + num);
+                    break;
+                case 3:
+                    Console.WriteLine("Num is - " + num);
+                    break;
+                default:
+                    Console.WriteLine("Whoops. Number < 5");
+                    break;
+            }
+
+           
+        }
+    }
+}
